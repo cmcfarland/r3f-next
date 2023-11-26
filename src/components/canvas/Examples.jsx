d6181bdea04a338ddfa979ef62d004d38008964f
cmcfarland <mcfarland.c.m@gmail.com>
2023-11-26 17:50:40 -0500

    basics up & running

--- src/components/canvas/Examples.jsx
+++ src/components/canvas/Examples.jsx
@@ -4,8 +4,10 @@ import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { useMemo, useRef, useState } from 'react'
-import { Line, useCursor, MeshDistortMaterial } from '@react-three/drei'
+import { Line, Plane, useCursor, MeshDistortMaterial } from '@react-three/drei'
+import { shaderMaterial } from '@react-three/drei'
 import { useRouter } from 'next/navigation'
+import { extend, Canvas } from '@react-three/fiber'
 
 export const Blob = ({ route = '/', ...props }) => {
   const router = useRouter()
@@ -35,18 +37,20 @@ export const Logo = ({ route = '/blob', ...props }) => {
     const t = state.clock.getElapsedTime()
     mesh.current.rotation.y = Math.sin(t) * (Math.PI / 8)
     mesh.current.rotation.x = Math.cos(t) * (Math.PI / 8)
-    mesh.current.rotation.z -= delta / 4
+    mesh.current.rotation.z -= delta / 2
   })
 
   return (
     <group ref={mesh} {...props}>
       {/* @ts-ignore */}
-      <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} />
+      <Line worldUnits points={points} color='#b2b21f' lineWidth={0.05} />
       {/* @ts-ignore */}
-      <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} rotation={[0, 0, 1]} />
+      <Line worldUnits points={points} color='#1ff5b2' lineWidth={0.05} rotation={[0, 0, 1]} />
       {/* @ts-ignore */}
-      <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} rotation={[0, 0, -1]} />
-      <mesh onClick={() => router.push(route)} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)}>
+      <Line worldUnits points={points} color='#f51ff5' lineWidth={0.05} rotation={[0, 0, -1]} />
+      <mesh onClick={() => router.push(route)}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}>
         <sphereGeometry args={[0.55, 64, 64]} />
         <meshPhysicalMaterial roughness={0} color={hovered ? 'hotpink' : '#1fb2f5'} />
       </mesh>
@@ -54,15 +58,56 @@ export const Logo = ({ route = '/blob', ...props }) => {
   )
 }
 
-export function Duck(props) {
-  const { scene } = useGLTF('/duck.glb')
+// FFT plane visualizer
+export const Spectrum = ({ halfLength = 512, defaultColor = '#b2b21f', ...props }) => {
+  // const halfLength = this.timeArray.length / 8;
+  const ColorShiftMaterial = shaderMaterial(
+    { time: 0, color: defaultColor },
+    /* vertex shader, glsl*/`
+      varying vec2 vUv;
+      void main() {
+        vUv = uv;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+    /* fragment shader, glsl*/`
+      uniform float time;
+      uniform vec3 color;
+      varying vec2 vUv;
+      #pragma glslify: random = require(glsl-random)
 
-  useFrame((state, delta) => (scene.rotation.y += delta))
+      void main() {
+        gl_FragColor.rgba = vec4(color + sin(time) * 0.2, 1.0);
+      }
+    `,
+  )
+  extend({ ColorShiftMaterial })
+  const mesh = useRef()
 
-  return <primitive object={scene} {...props} />
+  useFrame((state, delta) => {
+    const t = state.clock.getElapsedTime()
+    mesh.current.rotation.y = Math.sin(t) * (Math.PI / 8)
+    // mesh.current.rotation.x = Math.cos(t) * (Math.PI / 8)
+    mesh.current.rotation.z -= delta / 2
+    // mesh.current.color = 'green'
+  })
+
+  return (
+    // <Plane ref={mesh} args={[halfLength, halfLength]} color='#f51ff5' side={THREE.DoubleSide} {...props} />
+    <mesh ref={mesh}>
+      <boxGeometry args={[50, 50, 50]} />
+      <meshBasicMaterial color={0xff0000} wireframe />
+    </mesh>
+  )
 }
-export function Dog(props) {
-  const { scene } = useGLTF('/dog.glb')
 
-  return <primitive object={scene} {...props} />
-}
+const Cube = () => {
+  const mesh = useRef();
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={[100, 100, 100]} />
+      <meshBasicMaterial color={0xff0000} />
+    </mesh>
+  );
+};
--- src/components/canvas/View.jsx
+++ src/components/canvas/View.jsx
@@ -4,13 +4,15 @@ import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '@/helpers/components/Three'
 
-export const Common = ({ color }) => (
+export const Common = ({ color = null, posZ = 6, light = true }) => (
   <Suspense fallback={null}>
     {color && <color attach='background' args={[color]} />}
-    <ambientLight intensity={0.5} />
-    <pointLight position={[20, 30, 10]} intensity={1} />
-    <pointLight position={[-10, -10, -10]} color='blue' />
-    <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+
+    {light && <ambientLight intensity={0.5} />}
+    {light && <pointLight position={[20, 30, 10]} intensity={1} />}
+    {light && <pointLight position={[-10, -10, -10]} color='blue' />}
+
+    <PerspectiveCamera makeDefault fov={40} position={[0, 0, posZ]} />
   </Suspense>
 )
 
@@ -24,7 +26,7 @@ const View = forwardRef(({ children, orbit, ...props }, ref) => {
       <Three>
         <ViewImpl track={localRef}>
           {children}
-          {orbit && <OrbitControls />}
+          {orbit && <OrbitControls target={[0, 0, 0]} />}
         </ViewImpl>
       </Three>
     </>
